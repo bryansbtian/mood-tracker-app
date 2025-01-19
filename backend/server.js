7391cdcb23c03b289a2c3e3c986d4f40530abb69
bryansbtian <bryan.sebastian189@gmail.com>
2025-01-19 15:22:37 -0500

    changed new things 25

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,26 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://mood-tracker-app-client.vercel.app",
+    origin: "https://mood-tracker-app-client.vercel.app", // Client URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://mood-tracker-app-client.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = "secret";
