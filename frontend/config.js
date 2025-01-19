2525f2387d8216cba1b22a043c02eda908bb4e0c
bryansbtian <bryan.sebastian189@gmail.com>
2025-01-19 09:07:12 -0500

    changed new things 13

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,1 @@
-export const API_BASE_URL =
-  "https://mood-tracker-app-server-t1zqqcu31-bryan-susantos-projects.vercel.app";
+export const API_BASE_URL = "https://mood-tracker-app-server.vercel.app/";
